2fe20c252e44fddde1bfbd732da485da575cfd7e
Nat Pryce <sw@natpryce.com>
2017-02-21 17:31:18 +0000

    cross platform - macosx and linux

--- tests/migrate-date-format.sh
+++ tests/migrate-date-format.sh
@@ -1,6 +1,7 @@
 adr init
 adr_file="`adr new With Old Date Format`"
-sed -i "" -e 's+^Date: .*$+Date: 12/01/1992+' "$adr_file"
+sed -e 's+^Date: .*$+Date: 12/01/1992+' "$adr_file" > "$adr_file.tmp"
+mv "$adr_file.tmp" "$adr_file"
 adr new With Current Date Format
 grep 'Date: ' doc/adr/*
 adr upgrade-repository
