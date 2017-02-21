7da82322d46fa6c1b22abd1bcb1b79e4ac19c9c6
Nat Pryce <sw@natpryce.com>
2017-02-21 17:53:44 +0000

    grrr...

--- tests/migrate-date-format.sh
+++ tests/migrate-date-format.sh
@@ -1,5 +1,8 @@
 adr init
-adr_file="$(ADR_DATE=12/01/1992 adr new With Old Date Format)"
+(
+	ADR_DATE=12/01/1992
+	adr new With Old Date Format
+)
 adr new With Current Date Format
 grep Date: doc/adr/*
 adr upgrade-repository
