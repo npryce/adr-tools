3457d593f7d77c9015ee9f0cca773b9810980201
Nat Pryce <sw@natpryce.com>
2017-02-21 17:45:32 +0000

    more cross-platform woes

--- tests/migrate-date-format.sh
+++ tests/migrate-date-format.sh
@@ -1,8 +1,6 @@
 adr init
-adr_file="`adr new With Old Date Format`"
-sed -e 's+^Date: .*$+Date: 12/01/1992+' "$adr_file" > "$adr_file.tmp"
-mv "$adr_file.tmp" "$adr_file"
+adr_file="$(ADR_DATE=12/01/1992 adr new With Old Date Format)"
 adr new With Current Date Format
-grep 'Date: ' doc/adr/*
+grep Date: doc/adr/*
 adr upgrade-repository
-grep 'Date: ' doc/adr/*
+grep Date: doc/adr/*
