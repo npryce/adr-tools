51659f4d00b5a8ac6aaaff184ff2ba1109a9db8c
Nat Pryce <sw@natpryce.com>
2018-06-28 21:16:07 +0100

    Can pass -r <root> to the `adr generate graph` command to prefix links
    Partial implementation of #44

--- tests/generate-graph.sh
+++ tests/generate-graph.sh
@@ -3,7 +3,7 @@ adr new An idea that seems good at the time
 adr new -s 2 A better idea
 adr new This will work
 adr new -s 3 The end
-# with default extension in links
+# with default root and extension in links
 adr generate graph
-# with specified extension in links
-adr generate graph -e .xxx
+# with specified root and extension in links
+adr generate graph -r http://example.com/ -e .xxx
